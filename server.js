63dd7fa575e9af976eb2566731169866486d84e2
Caleb Hemphill <calebhemp@gmail.com>
2023-12-11 20:34:41 -0500

    Add book schema/model

--- server.js
+++ server.js
@@ -10,12 +10,10 @@ mongoose.connect('mongodb://127.0.0.1:27017/books');
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3005;
 
 app.get('/test', (request, response) => {
-
   response.send('test request received');
-
 });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
